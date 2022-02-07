307ad3002c84cf4c50452e785b2b5c7254480829
samaaehab <samaaehab48@gmail.com>
2022-02-07 18:17:05 +0200

    solveConflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+<<<<<<< HEAD
 import { WomenComponent } from './women/women.component';
 import { KidsComponent } from './kids/kids.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsUsComponent } from './contacts-us/contacts-us.component';
+=======
+import { AcountComponent } from './acount/acount.component';
+>>>>>>> a7dfe84 (signin&up)
 
 @NgModule({
   declarations: [
@@ -18,10 +22,14 @@ import { ContactsUsComponent } from './contacts-us/contacts-us.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
+<<<<<<< HEAD
     WomenComponent,
     KidsComponent,
     AboutComponent,
     ContactsUsComponent
+=======
+    AcountComponent
+>>>>>>> a7dfe84 (signin&up)
   ],
   imports: [
     BrowserModule,
